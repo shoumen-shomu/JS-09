2efc134c44f665fda4d11ee0ff8bdab79c7e1e5c
Shoumen <bshoumen@gmail.com>
2025-11-10 00:48:05 +0600

    c

--- js/script.js
+++ js/script.js
@@ -6,35 +6,99 @@ let error = document.getElementById("error");
 let errorMsg = document.getElementById("errorMsg");
 let success = document.getElementById("success");
 let secendPage = document.getElementById("secendPage");
+let logOut = document.getElementById("logOut");
+let addMoneyBtn = document.getElementById("addMoneyBtn");
+let cashoutBtn = document.getElementById("cashoutBtn");
+let transferMoneyBtn = document.getElementById("transferMoneyBtn");
+let getBonusBtn = document.getElementById("getBonusBtn");
+let payBillBtn = document.getElementById("payBillBtn");
+let transactionsBtn = document.getElementById("transactionsBtn");
+let transactionsPart = document.getElementById("transactionsPart");
+let addMoneyPart = document.getElementById("addMoneyPart");
+let cashOutPart = document.getElementById("cashOutPart");
+let transferMoneyPart = document.getElementById("transferMoneyPart");
+let getBonusPart = document.getElementById("getBonusPart");
+let payBillPart = document.getElementById("payBillPart");
+let commonBtn = document.querySelectorAll(".commonBtn");
 
 loginBtn.addEventListener("click", () => {
   let phoneNumber = number.value;
   let pinNumber = pin.value;
 
-   // Wrong number & pin
-  if (phoneNumber !== "12345" && pinNumber !== "123") {
+  // Wrong number & pin
+  if (phoneNumber !== "12345" && pinNumber !== "bolbona") {
     error.removeAttribute("hidden");
-  } 
-  else if (phoneNumber !== "12345") {
+  } else if (phoneNumber !== "12345") {
     errorMsg.innerHTML = "Wrong Number";
     error.removeAttribute("hidden");
-  } 
-  else if (pinNumber !== "123") {
+  } else if (pinNumber !== "bolbona") {
     errorMsg.innerHTML = "Wrong PIN";
     error.removeAttribute("hidden");
-  } 
-  else {
-    // correct number & pin 
+  } else {
+    // correct number & pin
     error.setAttribute("hidden", true);
     success.removeAttribute("hidden");
     secendPage.removeAttribute("hidden");
     fisrtPage.setAttribute("hidden", true);
-  
   }
   number.value = "";
   pin.value = "";
 });
 
+logOut.addEventListener("click", () => {
+  secendPage.setAttribute("hidden", true);
+  fisrtPage.removeAttribute("hidden");
+  success.setAttribute("hidden", true);
+});
+
+addMoneyBtn.addEventListener("click", () => {
+  transactionsPart.setAttribute("hidden", true);
+  cashOutPart.setAttribute("hidden", true);
+  transferMoneyPart.setAttribute("hidden", true);
+  getBonusPart.setAttribute("hidden", true);
+  payBillPart.setAttribute("hidden", true);
+  addMoneyPart.removeAttribute("hidden");
+});
+
+cashoutBtn.addEventListener("click", () => {
+  transactionsPart.setAttribute("hidden", true);
+  addMoneyPart.setAttribute("hidden", true);
+  transferMoneyPart.setAttribute("hidden", true);
+  getBonusPart.setAttribute("hidden", true);
+  payBillPart.setAttribute("hidden", true);
+  cashOutPart.removeAttribute("hidden");
+});
+
+transferMoneyBtn.addEventListener("click", () => {
+  transactionsPart.setAttribute("hidden", true);
+  addMoneyPart.setAttribute("hidden", true);
+  cashOutPart.setAttribute("hidden", true);
+  getBonusPart.setAttribute("hidden", true);
+  payBillPart.setAttribute("hidden", true);
+  transferMoneyPart.removeAttribute("hidden");
+});
+getBonusBtn.addEventListener("click", () => {
+  transactionsPart.setAttribute("hidden", true);
+  addMoneyPart.setAttribute("hidden", true);
+  cashOutPart.setAttribute("hidden", true);
+  transferMoneyPart.setAttribute("hidden", true);
+  payBillPart.setAttribute("hidden", true);
+  getBonusPart.removeAttribute("hidden");
+});
+payBillBtn.addEventListener("click", () => {
+  transactionsPart.setAttribute("hidden", true);
+  addMoneyPart.setAttribute("hidden", true);
+  cashOutPart.setAttribute("hidden", true);
+  transferMoneyPart.setAttribute("hidden", true);
+  getBonusPart.setAttribute("hidden", true);
+  payBillPart.removeAttribute("hidden");
+});
+
+commonBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    alert("Moner Takai Asol Taka");
+  });
+});
 
 // Another Method
 // let fisrtPage = document.getElementById("fisrtPage");
@@ -74,5 +138,3 @@ loginBtn.addEventListener("click", () => {
 //     fisrtPage.setAttribute("hidden", true);
 //   }
 // });
-
-
